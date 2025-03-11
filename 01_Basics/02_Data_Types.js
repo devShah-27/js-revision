a04b8171bc5c71b59d2df1dc710841ebea10fedf
Dev Shah <109164083+devShah-27@users.noreply.github.com>
2025-03-11 02:48:40 +0000

    revise datatypes in JS

---
+++ 01_Basics/02_Data_Types.js
@@ -0,0 +1,13 @@
+"use strict"; // treats all your JS code based on newer JS standards and features
+
+// There is no real reason to write this in modern JS files as most of the code is treated as strict by default.
+// However, it is a good practice to write this statement down for clarity.
+
+// alert(3 + 3); // does not work in node environment
+
+let varName = "Dev";
+
+console.log(typeof varName);
+console.log(typeof null); // returns "object" as null is an object in JS.
+console.log(typeof undefined); // returns "undefined" as it is a special type in JS.
+
